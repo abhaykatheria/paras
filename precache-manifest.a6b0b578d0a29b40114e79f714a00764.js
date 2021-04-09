52a19f0f2d23c8e4ae94d5274e465fef23c38a49
abhaykatheria <abhay.katheria1998@gmail.com>
2021-04-09 22:33:50 +0530

    Updates

---
+++ precache-manifest.a6b0b578d0a29b40114e79f714a00764.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe8cfdce23b74684c728b6c100fb30c",
+    "url": "/paras/index.html"
+  },
+  {
+    "revision": "7b5f3ede8dfc2357c035",
+    "url": "/paras/static/css/2.b71ebc9b.chunk.css"
+  },
+  {
+    "revision": "e48eedd2ed5616ee6ce4",
+    "url": "/paras/static/css/main.f8fd3397.chunk.css"
+  },
+  {
+    "revision": "7b5f3ede8dfc2357c035",
+    "url": "/paras/static/js/2.1f54b266.chunk.js"
+  },
+  {
+    "revision": "4ce0f5f7dc85bb55026a7e394d8e6086",
+    "url": "/paras/static/js/2.1f54b266.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e48eedd2ed5616ee6ce4",
+    "url": "/paras/static/js/main.80bc9e23.chunk.js"
+  },
+  {
+    "revision": "6744c916328453405cca",
+    "url": "/paras/static/js/runtime-main.40bf4659.js"
+  },
+  {
+    "revision": "eebcf226f3aa6aeed4edb7ad359eff13",
+    "url": "/paras/static/media/10853.eebcf226.jpg"
+  },
+  {
+    "revision": "87ebb3c314d8de8f54bd99c50b724066",
+    "url": "/paras/static/media/20944594.87ebb3c3.jpg"
+  },
+  {
+    "revision": "9e3e52c37a763c115b7ae873e13e9a21",
+    "url": "/paras/static/media/6265.9e3e52c3.jpg"
+  },
+  {
+    "revision": "5be8f5bca088a854ae8b1b7e0e8ce3aa",
+    "url": "/paras/static/media/CO2.5be8f5bc.jpg"
+  },
+  {
+    "revision": "add02758fb369a474328988c3c5c5d36",
+    "url": "/paras/static/media/alarm.add02758.png"
+  },
+  {
+    "revision": "41406a779fc77849c5e239173f4c4724",
+    "url": "/paras/static/media/arrow-left-3-icon.41406a77.svg"
+  },
+  {
+    "revision": "e5cd3c02a99df7a0698490d716530206",
+    "url": "/paras/static/media/arrow-right-3-icon.e5cd3c02.svg"
+  },
+  {
+    "revision": "65db3d1016872bd3c533b537e5b4b76f",
+    "url": "/paras/static/media/branchpipe.65db3d10.jpeg"
+  },
+  {
+    "revision": "8aa459ae951030ec44cb20c20de623c0",
+    "url": "/paras/static/media/briefcase.8aa459ae.svg"
+  },
+  {
+    "revision": "26d0d5756d8aa7c3deb5e641512bb67a",
+    "url": "/paras/static/media/bucket.26d0d575.jpg"
+  },
+  {
+    "revision": "d8705286beefd1ff7f7cf5a6a80c3a69",
+    "url": "/paras/static/media/car.d8705286.jpg"
+  },
+  {
+    "revision": "c66b213d1dfff391c61146727032f068",
+    "url": "/paras/static/media/chevron-down.c66b213d.svg"
+  },
+  {
+    "revision": "5f12b0dc993b9ea0bb9c928d98f89c07",
+    "url": "/paras/static/media/customers-logo-strip.5f12b0dc.png"
+  },
+  {
+    "revision": "7c1a6f97bf87de69a244342e6c17389c",
+    "url": "/paras/static/media/customize-icon.7c1a6f97.svg"
+  },
+  {
+    "revision": "23b66675f383d5a8f3c9281defa94096",
+    "url": "/paras/static/media/design.23b66675.jpg"
+  },
+  {
+    "revision": "cbeab2705cd1a8c5d774eac8ed9c9619",
+    "url": "/paras/static/media/dollar-sign.cbeab270.svg"
+  },
+  {
+    "revision": "1c251c8ff2222207650b4f24a2159cbb",
+    "url": "/paras/static/media/dot-pattern.1c251c8f.svg"
+  },
+  {
+    "revision": "a1ad24916d979e088c5e8c80b073f92b",
+    "url": "/paras/static/media/email-illustration.a1ad2491.svg"
+  },
+  {
+    "revision": "5e030c93ee64514b9fcdc887289ce8a9",
+    "url": "/paras/static/media/facebook-icon.5e030c93.svg"
+  },
+  {
+    "revision": "6c9e80eec6977f6497b0e55f0fce374a",
+    "url": "/paras/static/media/fast-icon.6c9e80ee.svg"
+  },
+  {
+    "revision": "b1fd9a5849817548fae9cc0aefd34387",
+    "url": "/paras/static/media/fireball.b1fd9a58.jpg"
+  },
+  {
+    "revision": "7dadab88be512218bdc9708a7453cc7c",
+    "url": "/paras/static/media/foam.7dadab88.jpg"
+  },
+  {
+    "revision": "bb059024af2f9a3c2ab5bf46d96a5ca9",
+    "url": "/paras/static/media/hosepipe.bb059024.jpg"
+  },
+  {
+    "revision": "df81b569e95e7dc962bceb8da22fe61c",
+    "url": "/paras/static/media/logo.df81b569.png"
+  },
+  {
+    "revision": "8304d83ccfeb652ad3afb85890c765a5",
+    "url": "/paras/static/media/lpgpipe.8304d83c.jpg"
+  },
+  {
+    "revision": "74bb31b01f0705ae4072bdc3cc94bb69",
+    "url": "/paras/static/media/mask.74bb31b0.jpg"
+  },
+  {
+    "revision": "e1a85f1a400d3e5695b9ae212bd9efb1",
+    "url": "/paras/static/media/mcp.e1a85f1a.jpg"
+  },
+  {
+    "revision": "ca394ec8a4754ab9db293dcac626bdcb",
+    "url": "/paras/static/media/menu.ca394ec8.svg"
+  },
+  {
+    "revision": "c1800dd7a8cb1451f498981a5abc15ae",
+    "url": "/paras/static/media/powder.c1800dd7.jpg"
+  },
+  {
+    "revision": "6cdf4661097076c679086cfce0d38d9a",
+    "url": "/paras/static/media/pressure.6cdf4661.jpg"
+  },
+  {
+    "revision": "952bb2383341eda1834e23285788a7a8",
+    "url": "/paras/static/media/refjacket.952bb238.jpg"
+  },
+  {
+    "revision": "a12572bdffec3f4a0b1df5e8a332a139",
+    "url": "/paras/static/media/reliable-icon.a12572bd.svg"
+  },
+  {
+    "revision": "dd82a6a8196ed76cdd482763abf2e46f",
+    "url": "/paras/static/media/rubberpipe.dd82a6a8.jpg"
+  },
+  {
+    "revision": "297d50a2cd8b864f0c1f357e5108d87a",
+    "url": "/paras/static/media/shield-icon.297d50a2.svg"
+  },
+  {
+    "revision": "493b428a5b7e948526a947a9e6306d2d",
+    "url": "/paras/static/media/shoes.493b428a.jpg"
+  },
+  {
+    "revision": "345871b443e4f5f841bedccf11a61859",
+    "url": "/paras/static/media/simple-icon.345871b4.svg"
+  },
+  {
+    "revision": "9191c6faa714437e5cb14969510eb652",
+    "url": "/paras/static/media/sprinkler.9191c6fa.jpg"
+  },
+  {
+    "revision": "5d0ff9bc5818d6498df9d90437127f90",
+    "url": "/paras/static/media/star-icon.5d0ff9bc.svg"
+  },
+  {
+    "revision": "135d403ccd52c50bf2e773a8a68cea57",
+    "url": "/paras/static/media/support-icon.135d403c.svg"
+  },
+  {
+    "revision": "ec52da39f670db5b4b3c32f54914a919",
+    "url": "/paras/static/media/svg-decorator-blob-1.ec52da39.svg"
+  },
+  {
+    "revision": "159be2476eedd8b63bb0c7e0a58d0e47",
+    "url": "/paras/static/media/svg-decorator-blob-3.159be247.svg"
+  },
+  {
+    "revision": "c86fb21a2396dc9a8149429f1cd451cf",
+    "url": "/paras/static/media/svg-decorator-blob-7.c86fb21a.svg"
+  },
+  {
+    "revision": "19df9c1d30e1ee23905d4034a5eb5da3",
+    "url": "/paras/static/media/svg-decorator-blob-8.19df9c1d.svg"
+  },
+  {
+    "revision": "06b7e9297b931c8f3c729d9666faf31f",
+    "url": "/paras/static/media/svg-decorator-blob-9.06b7e929.svg"
+  },
+  {
+    "revision": "5ae3fe7a0d10e5c14ec0575db276dcda",
+    "url": "/paras/static/media/team-illustration-2.5ae3fe7a.svg"
+  },
+  {
+    "revision": "7189052ca672d113f057d9a4f3cb5efe",
+    "url": "/paras/static/media/twitter-icon.7189052c.svg"
+  },
+  {
+    "revision": "751abb8fae88bce90248d8b360682792",
+    "url": "/paras/static/media/undraw_spread_love_r9jb.751abb8f.svg"
+  },
+  {
+    "revision": "217746efec2bf6b6701015bbd167718d",
+    "url": "/paras/static/media/water.217746ef.jpg"
+  },
+  {
+    "revision": "bd48e353be7f072b3bbaadecf26d2545",
+    "url": "/paras/static/media/wet-chemical.bd48e353.jpg"
+  },
+  {
+    "revision": "b0200bf7cbbb26c32989e7a0e4d19eef",
+    "url": "/paras/static/media/x.b0200bf7.svg"
+  },
+  {
+    "revision": "45e500e6c6c6577b063b28adc39fb422",
+    "url": "/paras/static/media/youtube-icon.45e500e6.svg"
+  }
+]);
